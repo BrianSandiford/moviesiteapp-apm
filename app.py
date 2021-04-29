67e807f08269cf62a82ed793e35ab027ccab34d2
briansandiford <rooms21@gmail.com>
2021-04-29 19:11:43 -0400

    added api_key variable to url string

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from  flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
@@ -8,10 +8,12 @@ app.config['DEBUG'] = True
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    if request.method == 'POST' :
+        movie_name = request.form.get('movie_name')
     api_key = '20acf4f9f1a3d619ed2764b51dd7a2f1'
-    url = 'https://api.themoviedb.org/3/search/movie?api_key=20acf4f9f1a3d619ed2764b51dd7a2f1&language=en-US&query={}&page=1&include_adult=false'
+    url = 'https://api.themoviedb.org/3/search/movie?api_key={}'.format(api_key) +'&language=en-US&query={}&page=1&include_adult=false'
     #url = 'https://api.tmdb.org/3/discover/movie/?api_key='+api_key 
-    movie_name = 'matrix'
+    #movie_name = 'matrix'
     r = requests.get(url.format(movie_name)).json()
     return  render_template('index.html', data=r['results'])
 
