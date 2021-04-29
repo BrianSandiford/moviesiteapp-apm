d70c0217e03417a400ddbd9fbd62361c281e137a
briansandiford <rooms21@gmail.com>
2021-04-29 19:34:21 -0400

    added condition for get request

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ app.config['DEBUG'] = True
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    api_key = '20acf4f9f1a3d619ed2764b51dd7a2f1'
     if request.method == 'POST' :
         movie_name = request.form.get('movie_name')
-    api_key = '20acf4f9f1a3d619ed2764b51dd7a2f1'
-    url = 'https://api.themoviedb.org/3/search/movie?api_key={}'.format(api_key) +'&language=en-US&query={}&page=1&include_adult=false'
-    #url = 'https://api.tmdb.org/3/discover/movie/?api_key='+api_key 
-    #movie_name = 'matrix'
-    r = requests.get(url.format(movie_name)).json()
+        url = 'https://api.themoviedb.org/3/search/movie?api_key={}'.format(api_key) +'&language=en-US&query={}&page=1&include_adult=false'
+        r = requests.get(url.format(movie_name)).json()
+    elif request.method == 'GET' :
+      url = 'https://api.themoviedb.org/3/movie/popular?api_key={}'.format(api_key) +'&language=en-US&page=1'
+      r = requests.get(url).json()
     return  render_template('index.html', data=r['results'])
 
 
