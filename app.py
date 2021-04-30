10c1b3b8d1e11fed7387375d6b4fb110fe7177c0
briansandiford <rooms21@gmail.com>
2021-04-30 18:55:12 -0400

    added condition for empty search

--- app.py
+++ app.py
@@ -11,9 +11,13 @@ def index():
     api_key = '20acf4f9f1a3d619ed2764b51dd7a2f1'
     if request.method == 'POST' :
         movie_name = request.form.get('movie_name')
-        url = 'https://api.themoviedb.org/3/search/movie?api_key={}'.format(api_key) +'&language=en-US&query={}&page=1&include_adult=false'
-        r = requests.get(url.format(movie_name)).json()
-    elif request.method == 'GET' :
+        if movie_name:
+             url = 'https://api.themoviedb.org/3/search/movie?api_key={}'.format(api_key) +'&language=en-US&query={}&page=1&include_adult=false'
+             r = requests.get(url.format(movie_name)).json()
+        else:
+          url = 'https://api.themoviedb.org/3/movie/popular?api_key={}'.format(api_key) +'&language=en-US&page=1'
+          r = requests.get(url).json()
+    else:
       url = 'https://api.themoviedb.org/3/movie/popular?api_key={}'.format(api_key) +'&language=en-US&page=1'
       r = requests.get(url).json()
     return  render_template('index.html', data=r['results'])
